f16804094b704bc8e508791f8d540995eadc5208
Praveen Kumar <praveenjillu17@gmail.com>
2025-04-03 20:56:03 +0530

    Add files via upload

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
@@ -8,6 +9,13 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True,max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+    # This is a unique identifier for the book, used in URLs
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.db.models import Avg
+
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all().order_by('-rating')
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating')) # rating__avg, rating__min, rating__max
+    
+    return render(request, 'book_outlet/index.html',{
+        'books': books,
+        "total_books": num_books,
+        "avg_rating": avg_rating['rating__avg'],
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id) #pk=primary key
+    # except:
+    #     raise Http404("Book not found")
+    book = get_object_or_404(Book, slug=slug) # This is a shortcut to handle the exception
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestselling': book.is_bestselling
+    })
